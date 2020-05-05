b2d6eb704778a9422c1873fc4af24c553091741f
marccbeltran <casiotonekid@gmail.com>
2020-05-05 22:36:29 +0200

    First Commit !

---
+++ Controllers/DataWeatherController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiWeather.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWeather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataWeatherController : ControllerBase
+    {
+        // GET: api/DataWeather
+        [HttpGet]
+        public IEnumerable<DataWeather> Get()
+        {
+            DataWeather data = new DataWeather();
+            yield return data;
+        }
+
+        // GET: api/DataWeather/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/DataWeather
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/DataWeather/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/DataWeather.cs
@@ -0,0 +1,11 @@
+namespace ApiWeather.Models
+{
+    public class DataWeather
+    {
+
+        public int Date { get; set; } = 122333;
+        public int Temp { get; set; } = 22;
+        public int Humidity { get; set; } = 80;
+
+    }
+}
